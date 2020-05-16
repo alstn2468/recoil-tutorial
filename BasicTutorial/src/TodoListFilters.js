18888b4a7434868bae102af3285a69a761a77568
alstn2468 <alstn2468_@naver.com>
2020-05-16 12:52:04 +0900

    Implement TodoListFilters component

--- BasicTutorial/src/TodoListFilters.js
+++ BasicTutorial/src/TodoListFilters.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { todoListFilterState } from './RecoilState';
 
 function TodoListFilters() {
-    return <div>TodoListFilters</div>;
+    const [filter, setFilter] = useRecoilState(todoListFilterState);
+
+    const updateFilter = ({ target: { value } }) => {
+        setFilter(value);
+    };
+
+    return (
+        <>
+            Filter:
+            <select value={filter} onChange={updateFilter}>
+                <option value="Show All">All</option>
+                <option value="Show Completed">Completed</option>
+                <option value="Show Uncompleted">Uncompleted</option>
+            </select>
+        </>
+    );
 }
 
 export default TodoListFilters;
