2646c3a98c6f760617bab86b6f3f81ba9dfbe896
alstn2468 <alstn2468_@naver.com>
2020-05-16 12:59:22 +0900

    Implement todoListStatsState using selector and TodoListStats component using useRecoilValue

--- BasicTutorial/src/RecoilState.js
+++ BasicTutorial/src/RecoilState.js
@@ -26,3 +26,23 @@ export const filteredTodoListState = selector({
         }
     },
 });
+
+export const todoListStatsState = selector({
+    key: 'todoListStatsState',
+    get: ({ get }) => {
+        const todoList = get(filteredTodoListState);
+        const totalNum = todoList.length;
+        const totalCompletedNum = todoList.filter((item) => item.isComplete)
+            .length;
+        const totalUncompletedNum = totalNum - totalCompletedNum;
+        const percentCompleted =
+            totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+
+        return {
+            totalNum,
+            totalCompletedNum,
+            totalUncompletedNum,
+            percentCompleted,
+        };
+    },
+});
--- BasicTutorial/src/TodoListStats.js
+++ BasicTutorial/src/TodoListStats.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { todoListStatsState } from './RecoilState';
 
 function TodoListStats() {
-    return <div>TodoListStats</div>;
+    const {
+        totalNum,
+        totalCompletedNum,
+        totalUncompletedNum,
+        percentCompleted,
+    } = useRecoilValue(todoListStatsState);
+
+    const formattedPercentCompleted = Math.round(percentCompleted * 100);
+
+    return (
+        <ul>
+            <li>Total items: {totalNum}</li>
+            <li>Items completed: {totalCompletedNum}</li>
+            <li>Items not completed: {totalUncompletedNum}</li>
+            <li>Percent completed: {formattedPercentCompleted}</li>
+        </ul>
+    );
 }
 
 export default TodoListStats;
